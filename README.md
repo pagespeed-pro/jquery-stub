8a8c3edf750951505259081a7593ed2d47691a50
optimalisatie <info@optimalisatie.nl>
2019-07-01 12:44:38 +0000

    0.0.1

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 Inline `jQuery(function(){})` and `jQuery.ready` queue to enable async loading of jQuery. It also queues `jQuery.noConflict`.
 
 Include `jquery-stub.js` before inline code that uses `jQuery`.
+
+When using [$async](https://github.com/style-tools/async) you can use `jquery-stub-async.js` to simply hook into the jQuery dependency or other dependencies.
+
+You could use `fn.toString()` and regular expression to selectively set the dependencies.
+
+```javascript
+$async.js({
+    src: '', // to save size, $async does not verify src and fails when omitted.
+    inline: callback_name + '();', 
+    dependencies: 'jquery', 
+    exec_timing: 'requestIdleCallback'
+});
+```
---
+++ jquery-stub-async.js
@@ -0,0 +1,65 @@
+/**
+ * jQuery Stub for inline jQuery()/.ready capture based on https://github.com/style-tools/async 
+ *
+ * Copyright (C) 2019 Style.Tools
+ * @link https://github.com/style-tools/jquery-stub
+ */
+
+(function(win, doc){
+
+    var JQUERY_CALLBACK_COUNT = 0;
+
+    // push to queue
+    function JQUERY_STUB_PUSH_QUEUE(handler, fn) {
+        fn = ((handler === "ready") ? fn : handler);
+
+        var index = ++JQUERY_CALLBACK_COUNT;
+        var callback_name = 'st_jq_cb_' + index;
+        win[callback_name] = function() {
+            jQuery(fn);
+        };
+
+        $async.js({
+            src: '', // to save size, $async does not verify src and fails when omitted.
+            inline: callback_name + '();', 
+            dependencies: 'jquery', 
+            exec_timing: 'requestIdleCallback'
+        });
+    };
+
+    // setup stub if jQuery is not yet loaded
+    if (!win.jQuery) {
+
+        var JQUERY_LOADED; // jQuery loaded flag
+
+        // jQuery.ready queue
+        var JQUERY_READY_QUEUE = [];
+        var JQUERY_NOCONFLICT = false;
+
+        // define an alias object 
+        var JQUERY_ALIAS_OBJECT = {
+            ready: JQUERY_STUB_PUSH_QUEUE,
+            bind: JQUERY_STUB_PUSH_QUEUE
+        };
+
+        // jQuery stub
+        win.$ = win.jQuery = function(handler) {
+
+            if (handler === doc || handler === undefined) {
+
+                // Queue $(doc).ready(handler), $().ready(handler)
+                // and $(doc).bind("ready", handler) by returning
+                // an object with alias methods for JQUERY_STUB_PUSH_QUEUE
+                return JQUERY_ALIAS_OBJECT;
+            } else {
+                // Queue $(handler)
+                JQUERY_STUB_PUSH_QUEUE(handler);
+            }
+        };
+
+        // mark stub
+        win.$.isStub = win.jQuery.isStub = true;
+
+    }
+
+}(window, document));
